1d43bb889dad975e739a659a70cf513c0e921961
Mohit Khandelwal <mohitkh7@gmail.com>
2022-09-15 12:44:03 +0530

    creates index page image uploader

---
+++ full-stack-developer/challenge1-image-uploader/backend-python/app.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask, request, render_template, send_from_directory
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = "temp/"
+
+app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+@app.get("/image/<string:image_id>")
+def get_image(image_id: str):
+	print(image_id)
+	resp = send_from_directory(app.config["UPLOAD_FOLDER"], image_id)
+	print(resp)
+	return resp
+
+@app.post("/image")
+def post_image(): 
+	image = request.files["image"]
+	filename = secure_filename(image.filename)
+	image.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+	return "uploaded!"
+
+@app.get("/")
+def home():
+	return render_template("index.html")

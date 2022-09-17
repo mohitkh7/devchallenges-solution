982be5a1bcf02ce569ca48b6002bc07c590a539a
Mohit Khandelwal <mohitkh7@gmail.com>
2022-09-17 15:53:13 +0530

    adds success and failure message screens

--- full-stack-developer/challenge1-image-uploader/backend-python/app.py
+++ full-stack-developer/challenge1-image-uploader/backend-python/app.py
@@ -1,26 +1,42 @@
 import os
-from flask import Flask, request, render_template, send_from_directory
+from flask import Flask, request, render_template, send_from_directory, redirect, url_for
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = "temp/"
+ALLOWED_EXTENSION = ('jpg', 'jpeg', 'png')
+MAX_FILE_SIZE = 1024 * 1024  # 1 MB
 
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = MAX_FILE_SIZE
 
-@app.get("/image/<string:image_id>")
-def get_image(image_id: str):
-	print(image_id)
-	resp = send_from_directory(app.config["UPLOAD_FOLDER"], image_id)
-	print(resp)
-	return resp
+@app.get("/image/<string:filename>")
+def image(filename: str):
+	return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
 
-@app.post("/image")
-def post_image(): 
+@app.post("/upload")
+def upload_image():
+	if "image" not in request.files:
+		return redirect(request.url)
 	image = request.files["image"]
+	if not image or image.filename == '':
+		return render_template("fail.html",
+			msg="No File Selected")
+	if not is_file_allowed(image.filename):
+		return render_template("fail.html",
+			msg="Invalid file format found. Upload only " + ', '.join(ext for ext in ALLOWED_EXTENSION))
 	filename = secure_filename(image.filename)
 	image.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
-	return render_template("success.html")
+	return render_template("success.html", image_id=filename)
 
 @app.get("/")
 def home():
 	return render_template("index.html")
+
+def is_file_allowed(filename):
+	return '.' in filename and \
+			filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSION
+
+def generate_id(length=6):
+	import random, string
+	return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
